099cb284170e36670bf1574d7dde259ea1511d48
prashant kumar <100068854+prashantkumar-creates@users.noreply.github.com>
2023-09-09 12:28:55 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,8 +2,15 @@ import mongoose from "mongoose";
 import colors from "colors"
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.Mongo_url);
+     try {
+        const conn = await mongoose.connect(process.env.Mongo_url
+            , {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                serverSelectionTimeoutMS: 5000, // Adjust the timeout as needed
+                autoReconnect: true,
+
+            });
         console.log(`connected to mongodb database ${conn.connection.host}`.bgRed);
     } catch (error) {
         console.log(`error in mongodb ${error}`.bgRed)
